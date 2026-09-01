0430f7c84c74732b6caecc665b06754ef6230cc5
KryptoVader <divyansh.shekhar2004@gmail.com>
2026-09-02 01:09:59 +0530

    Fixed The Mean Squarred Loss

--- Loss.java
+++ Loss.java
@@ -1,30 +1,31 @@
 public class Loss {
     public static double MSE(Matrix w, Matrix w_hat){
+        int N = w.shape()[0];
+        int O = w.shape()[1];
         double res = 0.0;
-        for(int i = 0; i < w.shape()[1]; i++){
-            res += Math.pow((w_hat.get(0,i)- w.get(0,i)),2);
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < O; j++){
+                res += Math.pow(w_hat.get(i,j) - w.get(i,j), 2);
+            }
         }
-        return res / (w.shape()[1] * 2);
+        return res / (2.0 * N * O);
     }
 
-    public static Matrix mseDerivative(Matrix w, Matrix w_hat, Matrix X) throws InvalidValue{
-        Matrix diff = new Matrix(w.shape()[0], w.shape()[1]);
-        for(int i = 0; i < w.shape()[1]; i++){
-            diff.set(0,i ,(w_hat.get(0,i)- w.get(0,i)));
-        }
-        Matrix res = new Matrix(X);
-        for(int i = 0; i < res.shape()[0]; i++){
-            for (int j = 0; j < res.shape()[1]; j++){
-                res.set(i, j, ((X.get(i,j) * diff.get(0,i)) / w.shape()[1]));
+    public static Matrix mseDerivative(Matrix w, Matrix w_hat, Matrix X) throws InvalidValue {
+        int N = w.shape()[0];
+        int O = w.shape()[1];
+        int F = X.shape()[1];
+        Matrix result = new Matrix(O, F);
+
+        for(int o = 0; o < O; o++){
+            for(int f = 0; f < F; f++){
+                double sum = 0.0;
+                for(int n = 0; n < N; n++){
+                    double error = w_hat.get(n,o) - w.get(n,o);
+                    sum += error * X.get(n,f);
+                }
+                result.set(o, f, sum / (N * O));
             }
-        }
-        Matrix result = new Matrix(1, X.shape()[1]);
-        for(int i = 0; i < X.shape()[1]; i++){
-            double sum = 0;
-            for(double ele: res.getCol(i)){
-                sum += ele;
-            }
-            result.set(0,i, sum);
         }
         return result;
     }
@@ -86,50 +87,5 @@ public class Loss {
             }
         }
         return new Matrix(Matrix.scalerMul(res, (1.0 / y.shape()[0])));
-    }
-    public static void main(String[] args) {
-        try {
-            Matrix y = new Matrix(3, 4);
-            y.set(0, 0, 1.0);
-            y.set(1, 2, 1.0); 
-            y.set(2, 1, 1.0);
-
-            Matrix p = new Matrix(3, 4);
-            p.set(0, 0, 0.7);
-            p.set(0, 1, 0.1);
-            p.set(0, 2, 0.1);
-            p.set(0, 3, 0.1);
-
-            p.set(1, 0, 0.1);
-            p.set(1, 1, 0.2);
-            p.set(1, 2, 0.6);
-            p.set(1, 3, 0.1);
-
-            p.set(2, 0, 0.1);
-            p.set(2, 1, 0.5);
-            p.set(2, 2, 0.2);
-            p.set(2, 3, 0.2);
-
-            System.out.println("y:");
-            System.out.println(y);
-            System.out.println("\np:");
-            System.out.println(p);
-
-            Matrix derivative = Loss.softmaxCrossEntropyDerivative(y, p);
-
-            System.out.println("\nSoftmax + CE derivative:");
-            System.out.println(derivative);
-            System.out.println("\nRow sums:");
-
-            for (int i = 0; i < derivative.shape()[0]; i++) {
-                double sum = 0.0;
-                for (int j = 0; j < derivative.shape()[1]; j++) {
-                    sum += derivative.get(i, j);
-                }
-                System.out.println("Row " + i + ": " + sum);
-            }
-        } catch (InvalidValue e) {
-            System.out.println(e.getMessage());
-        }
     }
 }
